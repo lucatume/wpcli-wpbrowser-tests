7580ccd4ca95c4b7d154e67a7508196331d877e4
Luca Tumedei <luca@theaveragedev.com>
2016-09-06 19:18:35 +0200

    more 5.3 arrray syntax

--- src/System/Composer.php
+++ src/System/Composer.php
@@ -18,22 +18,22 @@ class Composer {
 	protected function ensureGlobalComposer() {
 		exec( 'composer --version', $output, $return );
 		if ( $return !== 0 ) {
-			throw new MissingRequirementException( implode( "\n", [
+			throw new MissingRequirementException( implode( "\n", array(
 				"'composer' (https://getcomposer.org/) command not found or not good.",
 				'Either install Composer globally and make it available on your path (https://getcomposer.org/download/)',
 				'or specify its path using the --composer option.'
-			] ) );
+			) ) );
 		}
 	}
 
 	protected function ensureComposerPath( array $assocArgs ) {
 		exec( $assocArgs['composer'] . ' --version', $output, $return );
 		if ( $return !== 0 ) {
-			throw new MissingRequirementException( implode( "\n", [
+			throw new MissingRequirementException( implode( "\n", array(
 				"specified Composer path '{$assocArgs['composer']}' is not a valid Composer executable.",
 				'Either install Composer globally and make it available on your path (https://getcomposer.org/download/)',
 				'or specify its path using the --composer option.'
-			] ) );
+			) ) );
 		}
 	}
 }
