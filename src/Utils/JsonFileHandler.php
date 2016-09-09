078894749b0ce298ddc01dc042c23bf7a85b7f9b
theAverageDev (Luca Tumedei) <luca.tumedei@gmail.com>
2016-09-10 00:44:23 +0200

    Update JsonFileHandler.php

--- src/Utils/JsonFileHandler.php
+++ src/Utils/JsonFileHandler.php
@@ -79,13 +79,95 @@ class JsonFileHandler {
 			return true;
 		}
 
-//		if ( version_compare( PHP_VERSION, '5.4', '>=' ) ) {
-//			$contents = json_encode( $this->decodedContents, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES );
-//		} else {
-			$contents = str_replace( '\/', '/', json_encode( $this->decodedContents, JSON_PRETTY_PRINT ) );
-//		}
+		if ( version_compare( PHP_VERSION, '5.4', '>=' ) ) {
+			$contents = json_encode( $this->decodedContents, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES );
+		} else {
+			$contents = str_replace( '\/', '/', $this->json_readable_encode( $this->decodedContents ) );
+		}
 
 
 		return (bool) file_put_contents( $this->file, $contents );
 	}
+
+
+/*
+    json readable encode
+    basically, encode an array (or object) as a json string, but with indentation
+    so that i can be easily edited and read by a human
+
+    THIS REQUIRES PHP 5.3+
+
+    Copyleft (C) 2008-2011 BohwaZ <http://bohwaz.net/>
+
+    Licensed under the GNU AGPLv3
+*/
+
+function json_readable_encode($in, $indent = 0, Closure $_escape = null)
+{
+    if (__CLASS__ && isset($this))
+    {
+        $_myself = array($this, __FUNCTION__);
+    }
+    elseif (__CLASS__)
+    {
+        $_myself = array('self', __FUNCTION__);
+    }
+    else
+    {
+        $_myself = __FUNCTION__;
+    }
+
+    if (is_null($_escape))
+    {
+        $_escape = function ($str)
+        {
+            return str_replace(
+                array('\\', '"', "\n", "\r", "\b", "\f", "\t", '/', '\\\\u'),
+                array('\\\\', '\\"', "\\n", "\\r", "\\b", "\\f", "\\t", '\\/', '\\u'),
+                $str);
+        };
+    }
+
+    $out = '';
+
+    foreach ($in as $key=>$value)
+    {
+        $out .= str_repeat("\t", $indent + 1);
+        $out .= "\"".$_escape((string)$key)."\": ";
+
+        if (is_object($value) || is_array($value))
+        {
+            $out .= "\n";
+            $out .= call_user_func($_myself, $value, $indent + 1, $_escape);
+        }
+        elseif (is_bool($value))
+        {
+            $out .= $value ? 'true' : 'false';
+        }
+        elseif (is_null($value))
+        {
+            $out .= 'null';
+        }
+        elseif (is_string($value))
+        {
+            $out .= "\"" . $_escape($value) ."\"";
+        }
+        else
+        {
+            $out .= $value;
+        }
+
+        $out .= ",\n";
+    }
+
+    if (!empty($out))
+    {
+        $out = substr($out, 0, -2);
+    }
+
+    $out = str_repeat("\t", $indent) . "{\n" . $out;
+    $out .= "\n" . str_repeat("\t", $indent) . "}";
+
+    return $out;
+}
 }
